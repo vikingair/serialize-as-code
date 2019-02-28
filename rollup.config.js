4504f432187d28efec8212e99fe85498922144b1
Viktor Luft <viktor.luft@freiheit.com>
2019-02-28 13:16:35 +0100

    Fixed tests by using different babel configs

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,10 @@ import babel from 'rollup-plugin-babel';
 
 export default {
     input: 'src/index.js',
-    plugins: [babel({ exclude: 'node_modules/**' })],
+    plugins: [babel({
+            exclude: 'node_modules/**',
+            presets: [['@babel/preset-env', { modules: false, targets: { node: "8" } }]] }
+        )],
     output: [{
         dir: 'dist',
         format: 'cjs'
