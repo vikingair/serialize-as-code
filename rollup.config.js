e4d2c96a5684507c28d177cf6d3379ccd17cb233
Viktor Luft <viktor.luft@freiheit.com>
2019-03-01 11:45:10 +0100

    Bundling ESM together with CJS

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,20 @@
 import babel from 'rollup-plugin-babel';
+import flowEntry from 'rollup-plugin-flow-entry';
 
 export default {
     input: 'src/index.js',
-    plugins: [babel({
+    plugins: [
+        babel({
             exclude: 'node_modules/**',
             presets: [['@babel/preset-env', { modules: false, targets: { node: "8" } }]] }
-        )],
+        ),
+        flowEntry(),
+    ],
     output: [{
-        dir: 'dist',
+        dir: 'dist/cjs',
         format: 'cjs'
+    },{
+        dir: 'dist/esm',
+        format: 'esm'
     }]
 };
